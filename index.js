e5e4a4ba4710761cec6f9b6a46422fbdac5ccac8
TrineshCh <tchintan@gitam.in>
2026-03-05 15:41:32 +0530

    Uosjdhevfl

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-// Write your code here
 import {Component} from 'react'
 
 import './index.css'
 
 class ShowHide extends Component {
-  state = {isClick: true}
-  onClick = () => {
-    this.setState(prevState => ({isClick: !prevState.isClick}))
+  state = {
+    showFirstName: false,
+    showLastName: false,
   }
+
+  toggleFirstName = () => {
+    this.setState(prevState => ({
+      showFirstName: !prevState.showFirstName,
+    }))
+  }
+
+  toggleLastName = () => {
+    this.setState(prevState => ({
+      showLastName: !prevState.showLastName,
+    }))
+  }
+
   render() {
-    const {isClick} = this.state
+    const {showFirstName, showLastName} = this.state
+
     return (
       <div className="background">
         <h1 className="head">Show/Hide</h1>
+
         <div className="btn-cont">
-          <button className="btn" onClick={this.onClick} type="button">
+          <button className="btn" onClick={this.toggleFirstName} type="button">
             Show/Hide Firstname
           </button>
-          {isClick ? (
+
+          {showFirstName && (
             <div className="sub-cont">
               <p>Joe</p>
             </div>
-          ) : null}
-          ;
-          <button className="btn" onClick={this.onClick} type="button">
+          )}
+
+          <button className="btn" onClick={this.toggleLastName} type="button">
             Show/Hide Lastname
           </button>
-          {isClick ? (
+
+          {showLastName && (
             <div className="sub-cont">
               <p>Jonas</p>
             </div>
-          ) : null}
-          ;
+          )}
         </div>
       </div>
     )
   }
 }
+
 export default ShowHide
